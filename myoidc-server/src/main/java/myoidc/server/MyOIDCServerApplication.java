0db2a93c83399f12bb4356e78df01d9d4cb635e4
binge <3211528434@qq.com>
2023-04-19 16:16:59 +0800

    fix.修改日志

--- myoidc-server/src/main/java/myoidc/server/MyOIDCServerApplication.java
+++ myoidc-server/src/main/java/myoidc/server/MyOIDCServerApplication.java
@@ -1,12 +1,19 @@
 package myoidc.server;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * Spring Boot bootstrap
  */
 @SpringBootApplication
+@Slf4j
 public class MyOIDCServerApplication {
 
 
@@ -15,7 +22,19 @@ public class MyOIDCServerApplication {
      *
      * @param args args
      */
-    public static void main(String[] args) {
-        SpringApplication.run(MyOIDCServerApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(MyOIDCServerApplication.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isEmpty(path)) {
+            path = "";
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+            "Application  is running! Access URLs:\n\t" +
+            "Local访问网址: \t\thttp://localhost:" + port + path + "\n\t" +
+            "External访问网址: \thttp://" + ip + ":" + port + path + "\n\t" +
+            "----------------------------------------------------------");
     }
 }
